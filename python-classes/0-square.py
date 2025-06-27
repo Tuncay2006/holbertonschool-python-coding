21b69dba33f7c671d324725b0fbf9f9eeae0888a
Tuncay2006 <zenixthedark@gmail.com>
2025-06-27 01:57:23 +0000

    bn

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """This module defines a Square class."""
+
+
 class Square:
     """Class that defines a square by its size."""
+
     def __init__(self, size):
         self.__size = size
